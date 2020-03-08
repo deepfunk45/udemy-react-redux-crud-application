ba9d483597d8603610a92da23617d594444e4805
松井優太 <yuta.matsui@neuromagic.com>
2020-03-08 18:36:31 +0900

    learn prop-types

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 
 const App = () => {
   const profiles = [
   {
     name:"Taro",
-    age:10
+    age: 10
   },
   {
     name:"Hanako",
-    age:5
+    age: 5
   },
   {
-    name:" Noname"
+    name:" NoName", 
+    age: 3
   }
   ]
   return(
@@ -32,8 +34,9 @@ const User = (props) => {
   return <div>Hi, I am {props.name}, and {props.age} years old!</div>
 }
 
-User.defaultProps = {
-  age : 1
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired
 }
 
 export default App;
